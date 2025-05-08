aefc1859a5c593825950f5bf9e923ea1a0307cae
jsofiah <j.sofiah18@gmail.com>
2025-05-08 08:54:42 +0700

    Percobaan 1 modif

--- QueueMain21.java
+++ QueueMain21.java
@@ -19,22 +19,34 @@ public class QueueMain21 {
         int n = sc.nextInt();
         Queue21 Q = new Queue21(n);
         int pilih;
+        boolean berjalan = true;
 
         do{
             menu();
             pilih = sc.nextInt();
             switch (pilih) {
                 case 1:
-                    System.out.print("Masukkan data baru: ");
-                    int dataMasuk = sc.nextInt();
-                    Q.Enqueue(dataMasuk);
+                    if(Q.IsFull()){
+                        System.out.println("Queue Penuh. Program dihentikan");
+                        berjalan = false;
+                    } else{
+                        System.out.print("Masukkan data baru: ");
+                        int dataMasuk = sc.nextInt();
+                        Q.Enqueue(dataMasuk);
+                    }
                     break;
                 case 2:
-                    int dataKeluar = Q. Dequeue();
-                    if(dataKeluar != 0){
-                        System.out.println("Data yang dikeluarkan: " + dataKeluar);
-                        break;
+                    if(Q.IsEmpty()){
+                        System.out.println("Queue Kosong. Program dihentikan");
+                        berjalan = false;
+                    } else{
+                        int dataKeluar = Q. Dequeue();
+                        if(dataKeluar != 0){
+                            System.out.println("Data yang dikeluarkan: " + dataKeluar);
+                            break;
+                        }
                     }
+                    break;
                 case 3:
                     Q.print();
                     break;
@@ -45,6 +57,6 @@ public class QueueMain21 {
                     Q.clear();
                     break;
             }
-        } while(pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4 || pilih == 5);
+        } while(pilih >= 1 && pilih <= 5 && berjalan);
     }
 }
